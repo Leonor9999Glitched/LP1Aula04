a37369e8ccdb5e3e9ceb57b0b5371c5f821bc431
Leonor <leonorpatriciasf@hotmail.com>
2020-04-29 18:38:39 +0100

    My first commit

---
+++ MyFirstMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyFirstMethod
+{
+    class Program
+    {
+
+        static void CountTo10()
+        {
+            for (int i = 1 ; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CountToN(3);
+            CountTo10();
+        }
+
+        static void CountToN(int n1, int n2);
+        {
+            for (int i = n1 ; i <= n2; i ++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }    
+}
