bcea85a2227dd631af790e7e4a15841f66158918
Leonor <leonorpatriciasf@hotmail.com>
2021-03-17 10:39:59 +0000

    Documentação

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -5,6 +5,9 @@ namespace MyFirstMethod
     class Program
     {
 
+        /// <summary>
+        /// Prints numbers from 1 to 10
+        /// </summary>
         static void CountTo10()
         {
             for (int i = 1 ; i <= 10; i++)
@@ -19,6 +22,11 @@ namespace MyFirstMethod
             CountTo10();
         }
 
+        /// <summary>
+        /// Prints numbers from n1 to n1
+        /// </summary>
+        /// <param name="n1">valor minimo</param>
+        /// <param name="n2">valor maximo</param>
         static void CountToN(int n1, int n2);
         {
             for (int i = n1 ; i <= n2; i++)
